e630fb4c88c87644806bb63b2ec2a9ea4cd96067
Winnie Khuu <khuu.winnie@gmail.com>
2019-07-04 02:11:46 +0000

    Pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,9 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.order('created_at desc').paginate(:per_page => 5, :page => params[:page])
   end
+
+
 
 end
